68cece2a9f5a4e208d999102693c257a683617c4
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-01-25 14:37:29 +0600

    models.py

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from news.models import News
+
+admin.site.register(News)
+
 # Register your models here.
--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
+class News(models.Model):
+
+    class Meta:
+        verbose_name = 'новость'
+        verbose_name_plural = 'новости'
+
+    name = models.CharField(verbose_name='заголовок', max_length=100)
+    description = models.CharField(verbose_name='краткое описание', max_length=200)
+    content = models.TextField(verbose_name='контент')
+    date = models.DateTimeField(verbose_name='дата добавленения')
+
+    def __str__(self):
+        return f'{self.name} - {self.date}'
+
+
+
 # Create your models here.
