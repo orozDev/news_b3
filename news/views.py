e1af50fab6c657e1edefba711b1664f23509f50b
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-01-29 15:23:20 +0600

    templates, router, queries

--- news/views.py
+++ news/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from news.models import News
+
+
+def list_news(request):
+    news = News.objects.all()
+    return render(request, 'list_news.html', {'news': news})
+
+
+def main(request):
+    return render(request, 'index.html')
+
 # Create your views here.
--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from news.views import list_news, main
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name='main'),
+    path('news/', list_news, name='list_news')
 ]
