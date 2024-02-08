db02691bf81326d9f33fb9311e24e2cad53535c8
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-02-08 14:24:16 +0600

    foreign key

--- news/admin.py
+++ news/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from news.models import News, Category
+from news.models import News, Category, Tag
 
 admin.site.register(News)
 admin.site.register(Category)
+admin.site.register(Tag)
+# admin.site.register(NewsTags)
 
 # Register your models here.
--- news/models.py
+++ news/models.py
@@ -13,9 +13,19 @@ class News(models.Model):
     content = models.TextField(verbose_name='контент')
     date = models.DateTimeField(verbose_name='дата добавленения')
     category = models.ForeignKey('news.Category', on_delete=models.PROTECT, related_name='news', verbose_name='категория')
+    tags = models.ManyToManyField('news.Tag', related_name='news', verbose_name='теги', blank=True)
 
     def __str__(self):
         return f'{self.id}: {self.name} - {self.date}'
+
+
+# class NewsTags(models.Model):
+#
+#     news = models.ForeignKey('news.News', on_delete=models.CASCADE, related_name='tags')
+#     tag = models.ForeignKey('news.Tag', on_delete=models.CASCADE, related_name='news')
+#
+#     def __str__(self):
+#         return f'{self.news} - {self.tag}'
 
 
 class Category(models.Model):
@@ -24,11 +34,22 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
 
-    name = models.CharField(verbose_name='название категории', max_length=50)
+    name = models.CharField(verbose_name='название категории', max_length=50, unique=True)
 
     def __str__(self):
         return f'{self.id}: {self.name}'
 
+
+class Tag(models.Model):
+
+    class Meta:
+        verbose_name = 'тег'
+        verbose_name_plural = 'теги'
+
+    name = models.CharField(verbose_name='название тега', max_length=50, unique=True)
+
+    def __str__(self):
+        return f'{self.id}: {self.name}'
 
 
 # Create your models here.
