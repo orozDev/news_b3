adc9ced1ca39cdf3ee214165c13c8ddc03df06bc
xshxt <habibulaevhushbaht05@gmail.com>
2024-02-19 13:01:35 +0600

    news filter first day

--- news/views.py
+++ news/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
-from news.models import News
+from news.models import News,Category
 
 
 def list_news(request):
     news = News.objects.all()
-    return render(request, 'list_news.html', {'news': news})
+    categories = Category.objects.all()
 
+    selected_category_id = request.GET.get('category')
+
+    if selected_category_id:
+        news = news.filter(category__id=selected_category_id)
+
+    search_query = request.GET.get('search')
+
+    if search_query:
+        news = news.filter(name__icontains=search_query)
+
+    return render(request, 'list_news.html', {'news': news,'categories':categories})
 
 def main(request):
     return render(request, 'index.html')
