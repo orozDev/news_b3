3c0af3524de4883a7a3847671344831670f3fa23
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-03-27 13:57:35 +0600

    final

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-0$vol63xhbrxbjeunb+0zlx#eb%^!0fi5sn!k$lwgjq3-8g7#k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
