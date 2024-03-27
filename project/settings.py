77b6788d41d29f39fc6c762c568da143f1d67bf0
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-03-27 13:51:38 +0600

    final

--- project/settings.py
+++ project/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static', 'static_dirs'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
